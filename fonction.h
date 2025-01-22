b7b39af6759f741f1ab727e6dbe405b0c27ce7a9
malo-gueguen <maloguegbzh@gmail.com>
2025-01-22 11:00:54 +0100

    Create fonction.h

---
+++ fonction.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+
+#ifndef SDL_CESI_FUNCTION_H
+#define SDL_CESI_FUNCTION_H
+
+typedef struct Point {
+	int x;
+	int y;
+	int b;
+	int brique;
+} Point;
+typedef struct Balles {
+	int x;
+	int y;
+	int c;
+	int r;
+	int actif;
+} Balles;
+
+
+void init(int windowWidth, int windowHeight);
+
+void freeWindow();
+
+void freeRenderer();
+
+void freeTexture(SDL_Texture *texture);
+
+void freeAndTerminate();
+
+void actualize();
+
+void changeColor(int r, int g, int b);
+
+void clear();
+
+void drawSquare(int posX, int posY, int longueur);
+
+void drawRect(int posX, int posY, int longueur, int hauteur);
+
+void drawCircle(int centerX, int centerY, int rayon);
+
+void drawPoint(int x, int  y);
+
+void drawLine(int x0, int y0, int x1, int y1);
+
+void drawLinePoints(Point point1, Point point2);
+
+void sprite(int posX, int posY, char *imgSrc);
+
+void lastKeyPressed(SDL_Event *event);
+
+#endif //SDL_CESI_FUNCTION_H
+
+
